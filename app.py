97e66ec356d61a46aefb5132abea706a16582103
YURI TRIQUES DOS SANTOS <106687697+yuri07br@users.noreply.github.com>
2024-09-12 10:30:38 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-print('YT Mutimarcas')
+import os
+def mostrar titulo():
+print('''
+YT Mutimarcas
+   ''')
 
 print('1. cadastro de automoveis')
 print('2. listar automoveis')
@@ -8,6 +12,11 @@ print('4. sair da aplicaçao')
 opcao_escolhida = int (input('escolha uma opeção: '))
 print('voce escolheu a opção: ', opcao_escolhida)
 
+def finalizar_progama():
+    os.system('clear')
+    print('finalizando progama')
+
+
 if opcao_escolhida == 1 :
     print('cadastrar automoveis')
 elif opcao_escolhida == 2:
@@ -16,3 +25,7 @@ elif opcao_escolhida == 3:
     print('ativar/desativar narrador')
 else:
     print('voce escolheu a opeçao', opcao_escolhida)
+else:
+finalizar_progama()
+
+def main():
