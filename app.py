18bf53aef30cde22c8ada70804a3b13398d8e1e1
YURI TRIQUES DOS SANTOS <106687697+yuri07br@users.noreply.github.com>
2024-09-19 10:28:02 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -12,12 +12,36 @@ print('4. sair da aplicaçao')
 opcao_escolhida = int (input('escolha uma opeção: '))
 print('voce escolheu a opção: ', opcao_escolhida)
 
+def escolhe_opcao():
+    try:
+        opcao_escolhida = int(input('escolha uma opecao:'))
+        print('voce escolheu a opcao:'. opcao_escolhida)
+
+def cadatrar_automoveis() :
+    os.system('clear')
+    print('cadastrar automoveis...')
+    nome_automovel = input('digite o nome do automovel:')
+    automoveis.append(nome do carro )
+    print(f'{nome_carro}foi adicionado a Yt mutimarcas carros')
+    input('digite qualquer tecla para voltar')
+    main()
+    
+    def 
+
 def finalizar_progama():
     os.system('clear')
     print('finalizando progama')
 
+def opcao_invalida():
+    print('esse caracter nao e permitido')
+    input('digite qualquer tecla')
+    main()
+    
+    def main():
+        mostrar titulo()
+        
 
-if opcao_escolhida == 1 :
+if opcao_escolhida == 1:
     print('cadastrar automoveis')
 elif opcao_escolhida == 2:
     print('listar automoveis')
